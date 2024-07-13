a5fc58eef47dc706d2b988f0feb0d545eedb1b3f
maan3121 <radiman3121@gmail.com>
2024-07-14 01:09:10 +0700

    Reusable layout app

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,52 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-
-       
-        @vite(['resources/scss/app.scss', 'resources/js/app.js'])
-    </head>
-    <body >
-        <nav class="navbar navbar-dark navbar-expand-lg bg-warning">
-            <div class="container flex justify-content-between">
-              <a class="navbar-brand h-32px" href="#">Buchat</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mx-0 mx-lg-3">
-                  <li class="nav-item d-block d-lg-none d-xl-block">
-                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
-                  </li>
-                  <li class="nav-item ">
-                    <a class="nav-link "  href="#">Discussions</a>
-                  </li>
-                  <li class="nav-item ">
-                    <a class="nav-link text-nowrap"  href="#">About Us</a>
-                  </li>
-                 
-                </ul>
-                <form class="d-flex w-100 me-4 my-2 my-lg-0" role="search" action="#" method="GET">
-                    <div class="input-group">
-                        <span class="input-group-text bg-white border-end-0"><img src="{{ url('assets/images/magnifier.png') }}" alt=""></span>
-                        <input class="form-control border-start-0 ps-0" type="search" placeholder="Search" aria-label="Search" name="" value="">
-                    </div>
-                </form>
-                <ul class="navbar-nav ms-auto my-2 my-lg-0">
-                    <li class="nav-item my-auto">
-                        <a class="nav-link text-nowrap"  href="#">Log In</a>
-                    </li>
-                    <li class="nav-item ps-1 pe-0">
-                        <a class="btn btn-primary-white"  href="#">Sign Up</a>
-                    </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-
+@section('body')
           {{-- hero --}}
           <section class="container hero">
             <div class="row align-items-center h-100">
@@ -62,7 +16,6 @@
               </div>
             </div>
           </section>
-
           {{-- page promotion --}}
           <section class="container min-h-372px">
             <div class="row">
@@ -176,38 +129,10 @@
               <a href="#" class="btn btn-secondary mb-2 mb-lg-0">join Bual</a>
             </div>
           </section>
-          <footer class="py-80px">
-            <div class="container">
-              <div class="row">
-                <div class="col-12 col-lg-6 mb-5 mb-lg-0">
-                  <a class="h-32px mb-4" href="#">Buchat</a>
-                  <p class="text-white">menjalin kedekatan <br/> bersama Buchat</p>
-                </div>
-                <div class="col-12 col-lg-6 me-auto">
-                  <div class="d-flex flex-column flex-lg-row justify-content-end">
-                    <div class="d-flex flex-column me-140px mb-3 mb-lg-0">
-                    <p class="fw-bold fs-5 text-white text-nowrap">
-                      Contact Us
-                    </p>
-                    <ul class="list-unstyled">
-                      <li class="text-white"><a href="mailto:hi@buchat.com">hi@buchat.com</a></li>
-                    </ul>
-                    </div>
-                
-                  <div class="d-flex flex-column">
-                    <p class="fw-bold fs-5 text-white">Links</p>
-                    <ul class="list-unstyled">
-                      <li class="text-white mb-2"><a href="#">Home</a></li>
-                      <li class="text-white mb-2"><a href="#">Discussions</a></li>
-                      <li class="text-white mb-2"><a href="#">About Us</a></li>
-                    </ul>
-                  </div>
-                </div>
-                </div>
-              </div>
-            </div>
-          </footer>
+          
+ 
+@endsection
+    
 
-          <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    </body>
-</html>
+
+
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+      @include('partials.head')
+    </head>
+    <body >
+          {{-- NAVBAR --}}
+          @include('partials.nav')
+          @yield('body')
+          @include('partials.footer')
+          @yield('before-script')
+          @include('partials.script')
+          @yield('after-script')
+    </body>
+</html>
