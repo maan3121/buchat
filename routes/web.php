d71fcda510b6fc4db11b4d2f1a15ebaa687249e0
maan3121 <radiman3121@gmail.com>
2024-08-21 17:04:35 +0700

    membuat ui untuk mengedit profile

--- routes/web.php
+++ routes/web.php
@@ -44,3 +44,7 @@ Route::get('answers/1', function () {
 Route::get('users/radiman', function () {
     return view('pages.users.show');
 })->name('users.show');
+
+Route::get('users/radiman/edit', function () {
+    return view('pages.users.form');
+})->name('users.edit');
